969c8627d8059f69a398fa92cdff3b89a637a0a0
lingTemp <lyk_bj@163.com>
2020-11-05 14:15:18 +0800

    测试项目2

---
+++ t1/src/main/java/cn/com/controller/TestController.java
@@ -0,0 +1,19 @@
+package cn.com.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/hello")
+    public String helle(){
+
+
+        return "hello";
+    }
+
+
+}
